5a4b4005c004c432730dda731dda3f7aff9bf02c
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-13 00:53:24 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,29 @@
+var OSinfo = require('./modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		process.stdout.write(input);
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version':
+				process.stdout.write('Node version: ' + process.versions.node + '\n');
+				break;
+			case '/language':
+				process.stdout.write('Language: ' + process.env.LANG + '\n');
+				break;
+			case '/getOSinfo':
+    			OSinfo.print();
+		    	break;				
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
+
