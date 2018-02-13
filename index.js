98c6d7a8a1755c8d09e7c9bf61138a689b95ce32
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-13 01:52:15 +0100

    Update files

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-// var event = require('events');
-// var EventEmitter = event.EventEmitter;
-// alternative
-var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 
-var emitter = new EventEmitter();
-emitter.on('beforeCommand', function (instruction) {
-    console.log('You wrote: ' + instruction + ', trying to run command');
-});
-emitter.on('afterCommand', function () {
-    console.log('Finished command');
-});
-
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
 	if (input !== null) {
-		var instruction = input.trim();
-		emitter.emit('beforeCommand', instruction);
+		process.stdout.write(input);
+		var instruction = input.toString().trim();
 		switch (instruction) {
 			case '/exit':
 				process.stdout.write('Quitting app!\n');
@@ -34,8 +23,9 @@ process.stdin.on('readable', function() {
 		    	break;				
 			default:
 				process.stderr.write('Wrong instruction!\n');
-		};
-		emitter.emit('afterCommand');
+		}
 	}
 });
+
+
 
